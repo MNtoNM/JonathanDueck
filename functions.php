0c3def90fe7ac4980969d0e1e34c7697a1705d74
Jonathan Dueck <jdd612@gmail.com>
2018-07-02 21:43:28 -0600

    Clean up functions.php

--- functions.php
+++ functions.php
@@ -1,23 +1,23 @@
 <?php
 
-// require get_theme_file_path('/inc/functions-file-to-include.php');
+require get_theme_file_path('/inc/enqueue.php');
 
-function jd_portfolio_files() {
-  // wp_enqueue_script('jquery', get_theme_file_uri('/js/jquery.js'), array(), '3.3.1', true);
-  wp_deregister_script( 'jquery' );
-  	wp_register_script( 'jquery' , get_template_directory_uri() . '/js/jquery.js', true, '3.3.1', true );
-  	wp_enqueue_script( 'jquery' );
-
-  wp_enqueue_script('icons-js', get_theme_file_uri('/js/icons.js'), array('jquery'), microtime(), true);
-  wp_enqueue_style('google-custom-fonts', '//fonts.googleapis.com/css?family=Belgrano');
-  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', null, microtime());
-  wp_enqueue_style('firestarter_main_styles', get_stylesheet_uri());
-  wp_localize_script('main-firestarter-js', 'firestarterData', array(
-    'root_url' => get_site_url(),
-  ));
-}
-
-add_action('wp_enqueue_scripts', 'jd_portfolio_files');
+// function jd_portfolio_files() {
+//   // wp_enqueue_script('jquery', get_theme_file_uri('/js/jquery.js'), array(), '3.3.1', true);
+//   wp_deregister_script( 'jquery' );
+//   	wp_register_script( 'jquery' , get_template_directory_uri() . '/js/jquery.js', true, '3.3.1', true );
+//   	wp_enqueue_script( 'jquery' );
+//
+//   wp_enqueue_script('icons-js', get_theme_file_uri('/js/icons.js'), array('jquery'), microtime(), true);
+//   wp_enqueue_style('google-custom-fonts', '//fonts.googleapis.com/css?family=Belgrano');
+//   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', null, microtime());
+//   wp_enqueue_style('firestarter_main_styles', get_stylesheet_uri());
+//   wp_localize_script('main-firestarter-js', 'firestarterData', array(
+//     'root_url' => get_site_url(),
+//   ));
+// }
+//
+// add_action('wp_enqueue_scripts', 'jd_portfolio_files');
 
 function firestarter_custom_rest() {
   // Add a custom field 'authorName' to JSON response data:
--- js/icons.js
+++ js/icons.js
@@ -1,13 +1,10 @@
 $( document ).ready(function() {
   $(".fa").on("mouseover", (event) => {
     let moduleIcon = event.target;
-    console.log("ON: " + moduleIcon);
-    moduleIcon.animate({"font-size":"50px"}).delay(1300).removeClass("hide");
+    $(moduleIcon).animate({"font-size":"3em"}).delay(1300).removeClass("hide");
   });
   $(".fa").on("mouseout", (event) => {
     let moduleIcon = event.target;
-    console.log("OUT: " + moduleIcon);
-
-    // moduleIcon.addClass("hide");
+    $(moduleIcon).animate({"font-size": "2em"}) /*.addClass("hide");*/ ;
   });
 });
