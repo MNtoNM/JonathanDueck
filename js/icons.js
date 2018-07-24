de9d1533204f78c700f851d9ce42806a562e3cf3
Jonathan Dueck <jdd612@gmail.com>
2018-07-24 10:55:42 -0600

    Initiate contact form

--- js/icons.js
+++ js/icons.js
@@ -1,10 +1,10 @@
-$( document ).ready(function() {
-  $(".fa").on("mouseover", (event) => {
-    let moduleIcon = event.target;
-    $(moduleIcon).animate({"font-size":"3em"}).delay(1300).removeClass("hide");
-  });
-  $(".fa").on("mouseout", (event) => {
-    let moduleIcon = event.target;
-    $(moduleIcon).animate({"font-size": "2em"}) /*.addClass("hide");*/ ;
-  });
-});
+  // $( document ).ready(function() {
+  //   $(".fa").on("mouseover", (event) => {
+  //     let moduleIcon = event.target;
+  //     $(moduleIcon).animate({"font-size":"3em"}).delay(1300).removeClass("hide");
+  //   });
+  //   $(".fa").on("mouseout", (event) => {
+  //     let moduleIcon = event.target;
+  //     $(moduleIcon).animate({"font-size": "2em"}) /*.addClass("hide");*/ ;
+  //   });
+  // });
--- page-contact.php
+++ page-contact.php
@@ -6,5 +6,12 @@
 
   <h1>CONTACT</h1>
 
+<div class="form-component"
+  <form class="contact-form">
+    <input class="form-field" type="text" placeholder="Your Name" /><br />
+    <input class="form-field" type="email" placeholder="Email Address" /><br />
+    <textarea class="form-field" placeholder="Hey Jon! ..."></textarea>
+  </form>
+</div>
 
 <?php get_footer(); ?>
